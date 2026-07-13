d6266dd8fcd634efa4cd2749c33185a981660e32
Petar R <babananba@gmail.com>
2026-07-13 19:13:52 +0200

    Forsiranje https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if(app()->enviroment('production')){
-            URL::forceScheme('https';);
+            URL::forceScheme('https');
         }
         Gate::define('admin-access',function ($user){
              return $user->isAdmin;
