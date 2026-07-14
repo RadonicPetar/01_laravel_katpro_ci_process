2651a80cdb2b7caa5da59f03a209e4776f6a23fa
Petar R <babananba@gmail.com>
2026-07-14 19:16:18 +0200

    Test - grana

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(app()->environment('production')){
+        if(app()->environment('production') || app()->environment('test')){
             URL::forceScheme('https');
         }
         Gate::define('admin-access',function ($user){
