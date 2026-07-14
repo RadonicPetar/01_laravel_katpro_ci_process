22f285c075148e6f09e0a9e3ca2eadbb7422f780
Petar R <babananba@gmail.com>
2026-07-14 19:32:09 +0200

    educ - grana

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(app()->environment('production')){
+        if(app()->environment('educ')){
             URL::forceScheme('https');
         }
         Gate::define('admin-access',function ($user){
